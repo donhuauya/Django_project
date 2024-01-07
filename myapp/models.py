466619d82438975a3058ccfc7eda4053b915358a
huauya <donhuauya@gmail.com>
2024-01-07 21:49:20 +0100

    urls&database_succs

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class project(models.Model):
+    name = models.CharField(max_length = 200)
+
+class tasks(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    project = models.ForeignKey(project, on_delete=models.CASCADE)
